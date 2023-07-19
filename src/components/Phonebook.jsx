61d02c50b4930c9dc8f48a422bdeb8259c5fe26b
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-19 19:49:06 +0300

    Update Phonebook.jsx

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -8,15 +8,13 @@ export class Phonebook extends Component {
         name: this.props.name
     }
 
-    contactsData = [];
-
     addNewContact = async () => {
         try {
             const idD = await nanoid();
             const nameD = document.querySelector(".contactInput").value;
-            this.contactsData.push({id: idD, name: nameD});
-            //this.setState(state => ({contacts: [...state.contacts].push({id: id, name: name})}));
-            this.setState(state => ({contacts: this.contactsData}));
+            let contactsData = this.state.contacts;
+            contactsData.push({id: idD, name: nameD});
+            this.setState(state => ({contacts: contactsData}));
             console.log(this.state.contacts);
         }
         catch {
@@ -32,6 +30,8 @@ export class Phonebook extends Component {
                     className="contactInput"
                     type="text"
                     name="name"
+                    //pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
                     //pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     required
