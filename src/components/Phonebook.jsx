bf1f8722c8cde37ba50c08e1436572ec16dab262
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-19 16:37:42 +0300

    Update Phonebook.jsx

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -1,11 +1,11 @@
 import { Component } from "react";
-import propTypes from "prop-types";
-//import { nanoid } from 'nanoid/async'
-
+//import propTypes from "prop-types";
+//import { nanoid } from 'nanoid/async';
+/*
 async function createUser() {
     user.id = await nanoid();
 }
-
+*/
 export class Phonebook extends Component {
     state = {
         contacts: [],
@@ -14,15 +14,17 @@ export class Phonebook extends Component {
 
     render() {
         return(
-            <h2>Name</h2>
-            <input
-                type="text"
-                name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-            />
-            <button>Add contact</button>
+            <div>
+                <h2>Name</h2>
+                <input
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                />
+                <button>Add contact</button>
+            </div>
         );
     }
 }
