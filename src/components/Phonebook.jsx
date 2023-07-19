ff432937c611bc0de86c11b15ce88e1301571c11
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-19 15:53:55 +0300

    Update Phonebook.jsx

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -2,5 +2,17 @@ import { Component } from "react";
 import propTypes from "prop-types";
 
 class Phonebook extends Component {
-    
+
+
+    render() {
+        return(
+            <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+            />
+        );
+    }
 }
