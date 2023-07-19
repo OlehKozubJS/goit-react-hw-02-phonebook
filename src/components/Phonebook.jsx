305fc3869cc92b0efb43c830b9993fc6abf7b333
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-19 17:02:58 +0300

    Update Phonebook.jsx

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -1,15 +1,22 @@
 import { Component } from "react";
-//import propTypes from "prop-types";
-//import { nanoid } from 'nanoid/async';
-/*
-async function createUser() {
-    user.id = await nanoid();
-}
-*/
+import propTypes from "prop-types";
+import { nanoid } from 'nanoid/async';
+
 export class Phonebook extends Component {
     state = {
         contacts: [],
         name: this.props.name
+    }
+
+    addNewContact = async () => {
+        try {
+            const id = await nanoid();
+            const name = document.querySelector(".contactInput").value;
+            this.setState({contacts: this.state.contacts.push({id: id, name: name})});
+        }
+        catch {
+            console.log("Error!");
+        }
     }
 
     render() {
@@ -17,14 +24,19 @@ export class Phonebook extends Component {
             <div>
                 <h2>{this.state.name}</h2>
                 <input
+                    className="contactInput"
                     type="text"
                     name="name"
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     required
                 />
-                <button>Add contact</button>
+                <button onClick={this.addNewContact}>Add contact</button>
             </div>
         );
     }
 }
+
+Phonebook.propTypes = {
+    name: propTypes.string.isRequired
+};
