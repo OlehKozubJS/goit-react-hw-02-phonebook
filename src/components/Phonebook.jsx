4b2e70112519b71557ffc13f08ab86081c609ef4
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-19 16:32:11 +0300

    Update Phonebook.jsx

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -3,7 +3,7 @@ import propTypes from "prop-types";
 import { nanoid } from "nanoid";
 model.id = nanoid() //=> "V1StGXR8_Z5jdHi6B-myT"
 
-class Phonebook extends Component {
+export class Phonebook extends Component {
     state = {
         contacts: [],
         name: ''
