f895dbc94dde70c5c0126dc579d0ddc46f3f4265
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-20 21:40:15 +0300

    Update Phonebook.jsx

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -61,6 +61,15 @@ export class Phonebook extends Component {
                         {this.state.contacts.map(contact => <li key={contact.id}>{contact.name}</li>)}
                     </ul>
                 </label>
+                <label>
+                    <input
+                    type="tel"
+                    name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                    />
+                </label>
             </form>
         );
     }
