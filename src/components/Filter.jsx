cc09dc6cb448e6b14bc1a38eda6dbe76f557093a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-21 21:53:43 +0300

    Update Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,17 @@
 import { Component } from "react";
+import propTypes from "prop-types";
 
 export class Filter extends Component {
     render() {
         return (
             <div>
                 <h3>Find contacts by name</h3>
-                <input type="text" onChange={this.findContactsByName} />
+                <input type="text" onChange={this.props.changeFunction} />
             </div>
         )
     }
 } 
+
+Filter.propTypes = {
+    changeFunction: propTypes.func.isRequired
+};
