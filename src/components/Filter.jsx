f287a68791a317e2f6171f701d589df69705a52d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-21 22:26:15 +0300

    Update Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,15 +2,11 @@ import { Component } from "react";
 import propTypes from "prop-types";
 
 export class Filter extends Component {
-    state = {
-        inputValue: "",
-    };
-
     render() {
         return (
             <div>
                 <h3>Find contacts by name</h3>
-                <input type="text" value={this.state.inputValue} onChange={this.props.changeFunction} />
+                <input type="text" onChange={this.props.changeFunction} />
             </div>
         )
     }
