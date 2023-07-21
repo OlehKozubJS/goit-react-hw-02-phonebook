9a9f867b77ee72f687a0fb00021182441c442b01
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-21 19:57:24 +0300

    Update ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,41 +3,9 @@ import { Component } from "react";
 import { nanoid } from 'nanoid';
 
 export class Phonebook extends Component {
-    state = {
-        contacts: [
-            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-          ],
-        filter: "",
-        name: "",
-        number: ""
-    }
-
-    addNewContact = async event => {
-        event.preventDefault();
-
-        const {name, number} = event.currentTarget.elements;
-        const idData = nanoid();
-        const nameData = name.value;
-        const numberData = number.value;
-        
-        let contactsData = this.state.contacts;
-        contactsData.push({ id: idData, name: nameData, number: numberData });
-        this.setState({contacts: contactsData});
-        console.log(this.state.contacts);
-
-        event.currentTarget.reset();
-    }
-    
-    reset = () => {
-        this.setState({ filter: "", name: "", number: "" });
-    };
-
     render() {
         return(
-            <form className="phonebook" onSubmit={this.addNewContact}>
+            <form className="phonebook" onSubmit={this.props.submitFunction}>
                 <label>
                     <h3>Name</h3>
                     <input
