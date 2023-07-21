d419a587c94efc79e1a5197974e4152877754e39
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-21 20:00:55 +0300

    Update Phonebook.jsx

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { ContactForm } from "./ContactForm";
 //import propTypes from "prop-types";
 import { nanoid } from 'nanoid';
 
@@ -58,29 +59,7 @@ export class Phonebook extends Component {
         return(
             <div>
                 <h1>Phonebook</h1>
-                <form className="phonebook" onSubmit={this.addNewContact}>
-                    <label>
-                        <h3>Name</h3>
-                        <input
-                            type="text"
-                            name="name"
-                            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                            required
-                        />
-                    </label>
-                    <label>
-                        <h3>Number</h3>
-                        <input
-                            type="tel"
-                            name="number"
-                            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                            required
-                        />
-                    </label>
-                    <button type="submit">Add contact</button>
-                </form>
+                <ContactForm submitFunction={this.addNewContact} />
                 <div>
                     <h2>Contacts</h2>
                     <h3>Find contacts by name</h3>
