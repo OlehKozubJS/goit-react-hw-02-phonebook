cd29eb039dd954cee6d8599cf76105b3392f4719
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-21 18:17:56 +0300

    Update Phonebook.jsx

--- src/components/Phonebook.jsx
+++ src/components/Phonebook.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 //import propTypes from "prop-types";
 import { nanoid } from 'nanoid';
+//import { ReactDOM } from "react";
 
 export class Phonebook extends Component {
     state = {
