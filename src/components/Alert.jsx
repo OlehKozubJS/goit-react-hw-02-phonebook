c7a22b8509cb5967ab8cfbee9fd624e0f2af55f9
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-23 02:55:16 +0300

    Create Alert.jsx

---
+++ src/components/Alert.jsx
@@ -0,0 +1,13 @@
+import propTypes from "prop-types";
+import AlertStyles from "./PhonebookCSS/Alert.module.css";
+
+const Alert = (name) => {
+    return (
+        <div className={AlertStyles.alert}>{name} is already in contacts!</div>
+    );
+}
+
+Alert.propTypes = {
+    name: propTypes.string.isRequired
+}
+
