cc97d82edd21cda99dd85e2d37eaa6f043468cd7
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-23 01:15:24 +0300

    Update Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,13 @@
 import { Component } from "react";
 import propTypes from "prop-types";
+import FilterStyles from "./PhonebookCSS/Filter.module.css";
 
 export class Filter extends Component {
     render() {
         return (
             <label htmlFor="searchInput">
-                <h3>Find contacts by name</h3>
-                <input id="searchInput" type="text" onChange={this.props.changeFunction} />
+                <h3 className={FilterStyles.searchInputHeader}>Find contacts by name</h3>
+                <input type="text" id="searchInput"  className={FilterStyles.searchInput} onChange={this.props.changeFunction} />
             </label>
         )
     }
