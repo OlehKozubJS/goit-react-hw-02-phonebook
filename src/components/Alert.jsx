996ff948103145f26364a4d454e18df8aacdaf79
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-23 03:29:16 +0300

    Update Alert.jsx

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,12 +3,13 @@ import AlertStyles from "./PhonebookCSS/Alert.module.css";
 
 export const Alert = ({isInContacts, name}) => {
     return (
-        <div className={ isInContacts ? AlertStyles.alert : AlertStyles.hidden}>{name} is already in contacts!</div>
+        <div className={ isInContacts ? AlertStyles.alert : AlertStyles.hidden} onClick={this.props.clickFunction}>{name} is already in contacts!</div>
     );
 }
 
 Alert.propTypes = {
     isInContacts: propTypes.bool.isRequired,
+    clickFunction: propTypes.func.isRequired,
     name: propTypes.string.isRequired
 }
 
