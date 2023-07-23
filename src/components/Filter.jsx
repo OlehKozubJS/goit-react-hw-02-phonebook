295433c2f147571a728604e3bcf7f5ee22f701bd
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-24 00:15:09 +0300

    Update Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -5,7 +5,7 @@ export const Filter = ({changeFunction}) => {
     return (
         <label htmlFor="searchInput" className={FilterStyles.searchInputLabel}>
             <h3 className={FilterStyles.searchInputHeader}>Find contacts by name</h3>
-            <input type="text" id="searchInput"  className={FilterStyles.searchInput} onChange={changeFunction} />
+            <input type="text" id="searchInput"  className={FilterStyles.searchInput} onChange={event => changeFunction(event.currentTarget.value)} />
         </label>
     )
 } 
