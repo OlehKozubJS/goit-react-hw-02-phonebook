82a10144340a2be162187a3e2c10dc33a5f00974
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-24 15:47:29 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
-import { Phonebook } from "./Phonebook";
+import { Phonebook } from './Phonebook';
 
 export const App = () => {
-    return (
-        <div
-            style={{
-                height: '100vh',
-                display: 'flex',
-                justifyContent: 'center',
-                fontSize: 40,
-                color: '#010101'
-            }}
-        >
-              <Phonebook />
-        </div>
-    );
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Phonebook />
+    </div>
+  );
 };
